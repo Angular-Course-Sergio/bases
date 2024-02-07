c9572cbde9d415c0b13d0e5fe8b80fba2fe939b6
Sergio Barreras <sergiobg.isc@gmail.com>
2024-02-06 20:59:43 -0700

    ejercicio con componente hero terminado

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-heroes-hero',
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.css',
 })
 export class HeroComponent {
+  public name: string = 'ironman';
+  public age: number = 45;
 
+  get capitalizeName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${this.name} - ${this.age}`;
+  }
+
+  chageHero(): void {
+    this.name = 'wolverine';
+  }
+
+  changeAge(): void {
+    this.age = 30;
+  }
+
+  resetForm(): void {
+    this.name = 'ironman';
+    this.age = 45;
+  }
 }
