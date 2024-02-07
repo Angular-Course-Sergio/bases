d651cec971d8ce057d856e991eeaf9bd7f5db9c9
Sergio Barreras <sergiobg.isc@gmail.com>
2024-02-06 22:00:31 -0700

    separación por módulos

--- src/app/counter/components/counter/counter.component.ts
+++ src/app/counter/components/counter/counter.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
-  templateUrl: 'counter.component.html',
+  template: `<h3>Counter: {{ counter }}</h3>
+    <button (click)="increaseBy(+1)">+1</button>
+    <button (click)="resetCounter()">Reset</button>
+    <button (click)="increaseBy(-1)">-1</button>`,
 })
 export class CounterComponent {
   public counter: number = 10;
--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-heroes-list',
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.css',
 })
 export class ListComponent {
+  public heroNames: string[] = [
+    'Iron Man',
+    'Hulk',
+    'Black Widow',
+    'Thor',
+    'Hawkeye',
+    'Captain America',
+  ];
 
+  public deletedHero?: string;
+
+  removeLastHero(): void {
+    this.deletedHero = this.heroNames.pop();
+    console.log(this.heroNames.length);
+  }
 }
